79650df0e25c9ebac152c4a14a7d55c40dcd019f
Praktika1 <praktika1@connext.de>
2022-06-20 14:57:25 +0200

    5. Schritt mit Typescript (Add Navigation) FEHLERHAFT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages.component/messages.component.component';
+import { MessagesComponent } from './messages.component/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
--- src/app/messages.component/messages.component.ts
+++ src/app/messages.component/messages.component.ts
@@ -3,7 +3,7 @@ import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
-  templateUrl: './messages.component.component.html',
+  templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
